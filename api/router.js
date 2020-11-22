06e96509d1d1126c73b768d1a1cd91a7ed00746b
Hiroaki Date <mmst3l4@gmail.com>
2020-11-23 02:51:36 +0900

    apiサーバーを実装

---
+++ api/router.js
@@ -0,0 +1,34 @@
+const Express = require('express')
+
+const router = Express.Router();
+
+router.get('/boards', function(request, response, next) {
+  const boards = [
+    {
+      id: '1',
+      title: 'レーン１',
+      items: [
+        {id: '1', name: 'ほげほげ'},
+        {id: '2', name: 'いいいい'},
+      ]
+    },
+    {
+      id: '2',
+      title: 'レーン2',
+      items: [
+        {id: '3', name: 'うううう'},
+        {id: '4', name: 'ええええ'},
+      ]
+    },
+    {
+      id: '3',
+      title: 'レーン3',
+      items: [
+        {id: '5', name: 'おおおお'},
+      ]
+    },
+  ]
+  response.send(boards);
+});
+
+module.exports = router;
--- pages/index.vue
+++ pages/index.vue
@@ -10,39 +10,13 @@ import axios from '@nuxtjs/axios';
 
 export default Vue.extend({
 
-  // async fetch() {
-  //   this.boards = await fetch(
-  //     'http://localhost:8080/'
-  //   ).then(res => res.json());
-  // },
+  async fetch() {
+    this.boards = await fetch('http://localhost:3000/api/boards').then(res => res.json());
+  },
 
   data() {
     return {
-      boards: [
-        {
-          id: '1',
-          title: 'レーン１',
-          items: [
-            {id: '1', name: 'ほげほげ'},
-            {id: '2', name: 'いいいい'},
-          ]
-        },
-        {
-          id: '2',
-          title: 'レーン2',
-          items: [
-            {id: '3', name: 'うううう'},
-            {id: '4', name: 'ええええ'},
-          ]
-        },
-        {
-          id: '3',
-          title: 'レーン3',
-          items: [
-            {id: '5', name: 'おおおお'},
-          ]
-        },
-      ]
+      boards: null
     }
   },
 });
